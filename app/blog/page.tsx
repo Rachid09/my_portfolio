c628f33320b8afc90a1729c3e8403a904c39dc0c
rachid ahsoune <rahsoune@sekera-group.com>
2024-09-03 22:01:10 +0100

    Add sanity config and schemas blog for site

---
+++ app/blog/page.tsx
@@ -0,0 +1,12 @@
+import { Posts } from "@/components/posts";
+import { sanityFetch } from "@/sanity/lib/client";
+import { POSTS_QUERY } from "@/sanity/lib/queries";
+import { POSTS_QUERYResult } from "../../sanity.types";
+
+export default async function Page() {
+  const posts = await sanityFetch<POSTS_QUERYResult>({
+    query: POSTS_QUERY,
+  });
+
+  return <Posts posts={posts} />;
+}
---
+++ sanity.config.ts
@@ -0,0 +1,40 @@
+"use client";
+
+/**
+ * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.tsx` route
+ */
+
+import { visionTool } from "@sanity/vision";
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { presentationTool } from "sanity/presentation";
+
+// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import { apiVersion, dataset, projectId } from "./sanity/env";
+import { schema } from "./sanity/schemaTypes";
+import { structure } from "./sanity/structure";
+
+const SANITY_STUDIO_PREVIEW_URL =
+  process.env.SANITY_STUDIO_PREVIEW_URL || "http://localhost:3000/";
+
+export default defineConfig({
+  basePath: "/studio",
+  projectId,
+  dataset,
+  // Add and edit the content schema in the './sanity/schemaTypes' folder
+  schema,
+  plugins: [
+    structureTool({ structure }),
+    // Vision is for querying with GROQ from inside the Studio
+    // https://www.sanity.io/docs/the-vision-plugin
+    visionTool({ defaultApiVersion: apiVersion }),
+    presentationTool({
+      // Required: set the base URL to the preview location in the front end
+      previewUrl: {
+        draftMode: {
+          enable: `${SANITY_STUDIO_PREVIEW_URL}blog`,
+        },
+      },
+    }),
+  ],
+});
